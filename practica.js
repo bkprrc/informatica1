e41b7d907120a4c1f9233e28f2a56b72b783ac48
Ricardo Ruiz <bkprrc@gmail.com>
2021-06-11 10:33:52 -0500

    estos fueron los últimos detalles (11/06/2021).sara corrales)

--- practica.js
+++ practica.js
@@ -52,52 +52,83 @@ Multilinea*/
  //console.log (modulo, modulo2);
 // existen poreadores logicos en JavaScript.
 // Y = && conjuncion
-//O = !! disyuncion
+//O = || disyuncion
 //&&
 //true && true = true
 //true && false = false
 //false && true = false
 //false = false = false 
-//!!
+//||
 //true && true = true
 //true && false = true
 //false && true = true 
 //false = false = false 
-let condicion = 5;
-let condicon2 = 4;
-if (condicion>condicion2) {
-    console.log('Hola Mundo');
-}
-//crear un progarma que decida que operaciones hacer entre 2 numero 
-alert('HOLA OPERADOR');
-let nota = prompt('ESCRIBE UN NUMERO DE PREFERENCIA: ');
-let notas = parseInt(nota);
-console.log (nota*3);
-alert('CONTINUAR');
- let dato1 = prompt('INGRESE OTRO NUMERO');
- dato1 = parseInt(dato1);
- let dato2 = prompt('INGRESE OTRO NUMERO');
- dato2 = parseInt(dato2);
- let operacion = prompt('¿QUE OPERACION DECEAS REALIZAR? (adicion, sustracion, multiplicacion, division)');
+// let condicion = 5;
+// let condicon2 = 4;
+// if (condicion>condicion2) {
+//     console.log('Hola Mundo');
+// }
+// //crear un progarma que decida que operaciones hacer entre 2 numero 
+// alert('HOLA OPERADOR');
+// let nota = prompt('ESCRIBE UN NUMERO DE PREFERENCIA: ');
+// let notas = parseInt(nota);
+// console.log (nota*3);
+// alert('CONTINUAR');
+//  let dato1 = prompt('INGRESE OTRO NUMERO');
+//  dato1 = parseInt(dato1);
+//  let dato2 = prompt('INGRESE OTRO NUMERO');
+//  dato2 = parseInt(dato2);
+//  let operacion = prompt('¿QUE OPERACION DECEAS REALIZAR? (adicion, sustracion, multiplicacion, division)');
 
-    if (operacion == 'adicion') {
-    resultado = dato1+dato2;
-}
-else {
-    if (operacion == 'sustracion') {
-    resultado = dato1-dato2;
-}
-else {
-    if (operacion == 'multiplicacion') {
-    resultado = dato1*dato2;
-}
-else {
-    if (operacion == 'division') {
-    resultado = dato1/dato2;
-}
-}
-}
-}
+//     if (operacion == 'adicion') {
+//     resultado = dato1+dato2;
+//     alert("EL RESULTADO ES "+resultado);
+// }
+// else {
+//     if (operacion == 'sustracion' || operacion == 'divison '    ) {
+//     resultado = dato1-dato2;
+//     alert("EL RESULTADO ES "+resultado);
+// }
+// else {
+//     if (operacion == 'multiplicacion') {
+//     resultado = dato1*dato2;
+//     alert("EL RESULTADO ES "+resultado);
+// }
+// else {
+//     if (operacion == 'division') {
+//     resultado = dato1/dato2;
+//     alert("EL RESULTADO ES "+resultado);
+// }
+// }
+// }
+// }
 
-alert(resultado);
-alert('GRACIAS POR SU PARTICIPACION');
+// alert(resultado);
+// alert('GRACIAS POR SU PARTICIPACION');
+
+// operadores de comparacion
+// igual ==
+// igual y del mismo tipo ==
+// mayor o igual >=
+// menor igual <=
+// menor <
+// mayor >
+// diferente !=
+// diferente y distinto tipo !==
+
+// los arreglo es una coleccion de datos
+// asociar no es lo mismo que lista
+let nota = 5;
+let nota2 = 4;
+//arreglos 
+let arr = new Array(5);
+// arr,push(condicion2);
+ let arreglo = ['sara',3,nota, nota2];
+ for (let d = 4; d < 102; d++) {
+     let nota =arreglo [d];
+     console.log(nota);
+     console.log('SE AÑADIÔ LA NOTA AL ARREGLO ARR');
+     console.log(arr);
+     nota = nota*4;
+     console.log(nota);
+}
